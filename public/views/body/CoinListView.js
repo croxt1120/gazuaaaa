3625707e6b3cb05957e5adc6c5bc2597bf476a21
carran <carran@yescnc.co.kr>
2018-01-13 13:52:56 +0000

    number format

--- public/views/body/CoinListView.js
+++ public/views/body/CoinListView.js
@@ -54,6 +54,7 @@ define([
             this.$(".percent").text(percent + "%");
         },
         numberFormat : function(x){
+            x.toFixed(4);
             var parts = x.toString().split(".");
             parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             return parts.join(".");
